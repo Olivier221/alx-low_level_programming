1f1ba0734d8a7faccdb15d0883713198ff475437
Olivier221 <dusabamahoroolivier5@gmail.com>
2023-02-17 09:22:10 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0. followed by new line.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0.
  */
